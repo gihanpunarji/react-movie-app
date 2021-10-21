99307d75eac6bc10c8d7e067591a23f4f208a253
Gihan <gihanpunarji@gmail.com>
2021-10-21 10:51:01 +0530

    ADD: Grid Completed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -10,8 +10,24 @@ function Grid({ header, children }) {
     )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+    padding: 0 20px;
 
-const Content = styled.div``
+    h1 {
+        color: var(--medgray);
+
+        @media(max-width:768px) {
+            font-size: var(--fontBig);
+        }
+    }
+`
+
+const Content = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 2rem;
+`
 
 export default Grid
